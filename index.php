7ba158b82411ec38616aca7c4a668483574018ac
alecasentini <ale.casentini@gmail.com>
2023-05-19 14:33:02 +0200

    Stampa in tabella con bootstrap

--- index.php
+++ index.php
@@ -53,21 +53,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
 </head>
-<body>
+<body style="background-color: #F9FAFA;">
 
-<h1>Hotel</h1>
+    <h1 class="text-center my-5">Hotel</h1>
+    <div class="container bg-white">
 
-<?php
-for ($i = 0; $i < count($hotels); $i++) {
-    echo "Nome: " . $hotels[$i]['name'];
-    echo "Descrizione: " . $hotels[$i]['description'];
-    echo "Parcheggio: " . ($hotels[$i]['parking'] ? 'Si' : 'No');
-    echo "Voto: " . $hotels[$i]['vote'];
-    echo "Distanza dal centro: " . $hotels[$i]['distance_to_center'] . "Km";
-}
-?>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" >Nome</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col" class="text-center">Parcheggio</th>
+                    <th scope="col" class="text-center">Voto</th>
+                    <th scope="col" class="text-end">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    for ($i = 0; $i < count($hotels); $i++) {
+                        echo "<tr>";
+                            echo '<td>' . $hotels[$i]['name'] . "</td>";
+                            echo '<td>' . $hotels[$i]['description'] . "</td>";
+                            echo '<td class="text-center">' . ($hotels[$i]['parking'] ? 'Si' : 'No') . "</td>";
+                            echo '<td class="text-center">' . $hotels[$i]['vote'];
+                            echo '<td class="text-end">' . $hotels[$i]['distance_to_center'] . "Km </td>";
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
 
-    
+    </div>    
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
